ce5b1b21712483783fa8edf2a13eac895855c67a
Bijan Roy <bijan.roy@sakhaglobal.com>
2024-07-25 15:01:54 +0530

    first test api 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    re.send("Hello world Bijan");
+    res.send("Hello world Bijan");
 })
 
 app.get('/api', (req, res) => {
